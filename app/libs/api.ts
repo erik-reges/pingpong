20d820cddcccf0e2a12413462297de813aada245
erik-reges <erik.celander@reges.se>
2024-11-22 14:36:57 +0100

    stuff

--- app/libs/api.ts
+++ app/libs/api.ts
@@ -1,13 +1,14 @@
 import { treaty } from "@elysiajs/eden";
 import { API } from "../api/[[...slugs]]/route";
 
-export const api = treaty<API>(
-  typeof window === "undefined"
-    ? `http://localhost:${process.env.PORT ?? 3000}`
-    : window.location.origin,
-  {
-    fetch: {
-      cache: "no-store",
-    },
+const getBaseUrl = () => {
+  if (typeof window !== "undefined") return ""; // browser should use relative url
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+  return `http://localhost:${process.env.PORT ?? 3000}`;
+};
+
+export const api = treaty<API>(getBaseUrl(), {
+  fetch: {
+    cache: "no-store",
   },
-).api;
+}).api;
